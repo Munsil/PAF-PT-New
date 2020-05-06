ec50a3f3e2667f31b824e1cf732e238d3891a767
MuRemo <munzirramo18@gmail.com>
2020-05-06 18:00:02 +0530

    Table & read from database

--- HospiatalManagement/src/main/java/com/hospital/HospiatalManagement/HospitalService.java
+++ HospiatalManagement/src/main/java/com/hospital/HospiatalManagement/HospitalService.java
@@ -1,5 +1,32 @@
 package com.hospital.HospiatalManagement;
 
-public class HospitalService {
 
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+
+import model.Hospital;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+@Path("/Hospitals")
+public class HospitalService
+{
+	Hospital HospitalObj = new Hospital();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems()
+	{
+		return HospitalObj.readHospital();
+	}
+	
+	
+
+	
 }
+
--- HospiatalManagement/src/main/java/model/Hospital.java
+++ HospiatalManagement/src/main/java/model/Hospital.java
@@ -9,16 +9,16 @@ public class Hospital
 						 Connection con = null;
 						 try
 						 {
-						 Class.forName("com.mysql.jdbc.Driver");
+						 Class.forName("com.mysql.cj.jdbc.Driver");
 						
 						 //Provide the correct details: DBServer/DBName, username, password
-						 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+						 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/testpaf?serverTimezone=UTC", "root", "");
 						 }
 						 catch (Exception e)
 						 {e.printStackTrace();}
 						 return con;
 				 }
-		public String insertHospital(String code, String name, String price, String desc)
+		public String insertHospital(String name, String address, String charge, String phonenumber, String roomcount)
 			{
 				 String output = "";
 				 try
@@ -29,17 +29,19 @@ public class Hospital
 						 {return "Error while connecting to the database for inserting."; }
 						 
 						 // create a prepared statement
-						 String query = " insert into items
-						 (`itemID`,`itemCode`,`itemName`,`itemPrice`,`itemDesc`)"
-						 + " values (?, ?, ?, ?, ?)";
+						 String query = " insert into hospital(`hospitalid`,`name`,`address`,`charge`,`phonenumber`, 'roomcount')values (?, ?, ?, ?, ? ,?)";
+						 
+					
+					
 						
 						 PreparedStatement preparedStmt = con.prepareStatement(query);
 						 // binding values
 						 preparedStmt.setInt(1, 0);
-						 preparedStmt.setString(2, code);
-						 preparedStmt.setString(3, name);
-						 preparedStmt.setDouble(4, Double.parseDouble(price));
-						 preparedStmt.setString(5, desc);
+						 preparedStmt.setString(2, name);
+						 preparedStmt.setString(3, address);
+						 preparedStmt.setDouble(4, Double.parseDouble(charge));
+						 preparedStmt.setString(5, phonenumber);
+						 preparedStmt.setInt(6, Integer.parseInt(roomcount));
 						// execute the statement
 						 preparedStmt.execute();
 						 con.close();
@@ -55,7 +57,7 @@ public class Hospital
 		}
 						
 		
-	public String readItems()
+	public String readHospital()
 	{
 				String output = "";
 				
@@ -67,33 +69,36 @@ public class Hospital
 						 {return "Error while connecting to the database for reading."; }
 						 
 						 // Prepare the html table to be displayed
-						 output = "<table border=\"1\"><tr><th>Item Code</th><th>Item Name</th><th>Item"
-								 	+ "Price</th><th>Item"
-								 	+ "Description</th><th>Update</th><th>Remove</th></tr>";
+						 output = "<table border=\"1\"><tr><th>ID</th><th>Name</th><th>Adress</th><th>Charge</th><th>Phone Number</th><th>Room Count</th><th>Update</th><th>Remove</th></tr>";
+								 	
 						
-						 String query = "select * from items";
+						 String query = "select * from hospital";
 						 Statement stmt = con.createStatement();
 						 ResultSet rs = stmt.executeQuery(query);
 						 // iterate through the rows in the result set
 						 while (rs.next())
 						 {
-						 String itemID = Integer.toString(rs.getInt("itemID"));
-						 String itemCode = rs.getString("itemCode");
-						 String itemName = rs.getString("itemName");
-						 String itemPrice = Double.toString(rs.getDouble("itemPrice"));
-						 String itemDesc = rs.getString("itemDesc");
+						 String hospitalID = Integer.toString(rs.getInt("hospitalid"));
+						 String hospitalName = rs.getString("name");
+						 String hospitalAddress = rs.getString("address");
+						 String hospitalCharge = Double.toString(rs.getDouble("charge"));
+						 String hospitalPhone = rs.getString("phonenumber");
+						 String hospitalcount = Integer.toString(rs.getInt("roomcount"));
 						 // Add into the html table
-						 output += "<tr><td>" + itemCode + "</td>";
-						 output += "<td>" + itemName + "</td>";
-						 output += "<td>" + itemPrice + "</td>";
-						 output += "<td>" + itemDesc + "</td>";
+						 output += "<tr><td>" + hospitalID + "</td>";
+						 output += "<td>" + hospitalName + "</td>";
+						 output += "<td>" + hospitalAddress + "</td>";
+						 output += "<td>" + hospitalCharge + "</td>";
+						 output += "<td>" + hospitalPhone + "</td>";
+						 output += "<td>" + hospitalcount + "</td>";
+						 
 						 // buttons
-						 output += "<td><input name=\"btnUpdate\" type=\"button\"
+						 output += "<td><input name=\"btnUpdate\" type=\"button\""
 						 +"value=\"Update\" class=\"btn btn-secondary\"></td>"
-						 + "<td><form method=\"post\" action=\"items.jsp\">"
-						 + "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\"
-						 class=\"btn btn-danger\">"
-						 + "<input name=\"itemID\" type=\"hidden\" value=\"" + itemID
+						 + "<td><form method=\"post\" action=\"hospital.jsp\">"
+						 + "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\""
+						 + "class=\"btn btn-danger\">"
+						 + "<input name=\"itemID\" type=\"hidden\" value=\"" + hospitalID
 						 + "\">" + "</form></td></tr>";
 						 }
 						 con.close();
@@ -107,3 +112,62 @@ public class Hospital
 					}
 				 return output;
 			} 
+	
+	public String updateHospital(String hospitalid, String name, String address, String charge, String phonenumber, String roomcount)
+			 {
+			 	String output = "";
+			 try
+			 {
+				 Connection con = connect();
+					 if (con == null)
+					 {return "Error while connecting to the database for updating."; }
+					 // create a prepared statement
+					 String query = "UPDATE hospital SET name=?,address=?,charge=?,phonenumber=?,roomcount=? WHERE hospitalid=?";
+					 
+					 PreparedStatement preparedStmt = con.prepareStatement(query);
+					 // binding values
+					 preparedStmt.setString(1, name);
+					 preparedStmt.setString(2, address);
+					 preparedStmt.setDouble(3, Double.parseDouble(charge));
+					 preparedStmt.setString(4, phonenumber);
+					 preparedStmt.setInt(5, Integer.parseInt(roomcount));
+					 // execute the statement
+					 preparedStmt.execute();
+					 con.close();
+					 output = "Updated successfully";
+					 }
+			 catch (Exception e)
+			 {
+			 output = "Error while updating the item.";
+			 System.err.println(e.getMessage());
+			 }
+			 return output;
+			 }
+	
+	
+	public String deleteHospital(String hospitalid)
+			 {
+			 		String output = "";
+			 try
+			 {
+				 	Connection con = connect();
+							 if (con == null)
+							 {return "Error while connecting to the database for deleting."; }
+							 // create a prepared statement
+							 String query = "delete from hospital where hospitlid=?";
+							 PreparedStatement preparedStmt = con.prepareStatement(query);
+							 // binding values
+							 preparedStmt.setInt(1, Integer.parseInt(hospitalid));
+							 // execute the statement
+							 preparedStmt.execute();
+							 con.close();
+							 output = "Deleted successfully";
+							 }
+			 catch (Exception e)
+			 {
+					 output = "Error while deleting the item.";
+					 System.err.println(e.getMessage());
+			 }
+			 return output;
+			 }
+	} 
